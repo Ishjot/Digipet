e495bb89f29dd6e7bdc65ba34bf66eae0bbbbdc9
Ishjot Walia <ishjotwalia@umail.ucsb.edu>
2017-02-05 21:36:01 -0800

    Beginning work on GUI

--- src/Digipet.java
+++ src/Digipet.java
@@ -1,8 +1,37 @@
-public class Digipet {
+import javax.swing.*;
+import java.awt.event.*;
+
+/*
+ * The main GUI for the application
+ */
+public class Digipet implements ActionListener {
+	JFrame frame;
+	JButton button;
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-		System.out.println("Hello, again!");
+		Digipet gui = new Digipet();
+		gui.InitGui();
 	}
 
+	public Digipet() {
+		JFrame frame = new JFrame();
+		JButton button = new JButton("Play");
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(button);
+		frame.setSize(300,300);
+		frame.setVisible(true);
+	}
+
+	public void InitGui() {
+		frame = new JFrame();
+		button = new JButton("Play");
+		
+		button.addActionListener(this);
+
+		frame.setDefaultCloserOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(button);
+		frame.setSize(600, 600);
+		frame.setVisible(true);
+	}
 }
