8614199a636085be7659be23afbbc1eac0ce3d4d
calvinyoung <calvinyoung14@gmail.com>
2017-02-12 01:15:00 -0800

    adsfasdf

--- src/Models/player.java
+++ src/Models/player.java
@@ -3,5 +3,29 @@
  */
 public class player {
 
+    Pet playerPet;
+    int currency;
+    Item[] iventory = new Item[20]; //changeable size depending on balance
+
+
+    public void feedPet() {
+        return;
+    }
+
+    public abstract void petPlay();
+
+    public void sellItem(Item trash){
+        return;
+    }
+
+    public void buyItem(Item resource){
+        return;
+    }
+
+    //this is probably just the "work"
+    public void playGame(){
+        return;
+    }
+
 
 }
