1b06b0c2af936bbd578de8714855fbfc5a544476
Noel Vargas <n_vargas@csil.cs.ucsb.edu>
2017-02-14 17:57:41 -0800

    Added CleanItem abstract class

---
+++ src/Models/CleanItem.java
@@ -0,0 +1,13 @@
+public abstract class CleanItem extends Item{
+    private int cleanRate = 0;
+
+    int getCleanRate(){
+	return cleanRate;
+    }
+
+    void setCleanRate(int rate){
+	this.cleanRate = rate;
+    }
+
+
+}
