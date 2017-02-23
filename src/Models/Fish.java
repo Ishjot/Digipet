23da7e801f377edd9b8e934bcfe2358bbf704dd0
Jeffrey Liu <jeffreyliu@umail.ucsb.edu>
2017-02-22 23:20:14 -0800

    changed fish to salmon

--- src/Models/Fish.java
+++
@@ -1,5 +0,0 @@
-public class Fish extends Food {
-    public Fish() {
-	super("Fish", 15, 10, 1, "");
-    }
-}
--- src/Models/ItemList.java
+++ src/Models/ItemList.java
@@ -4,7 +4,7 @@ public class ItemList {
     private ArrayList<Item> items = new ArrayList<Item>();
     public ItemList() {
 	items.add(0, new Biscuit());
-	items.add(new Fish());
+	items.add(new Salmon());
     }
     public Item getItem(int i) {
 	return items.get(i);
