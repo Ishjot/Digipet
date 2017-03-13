b3597359141e0bdae9a1a38b007816c93993f230
Jun Hong Peng <apengjh@gmail.com>
2017-03-13 14:57:35 -0700

    comments @ javadoc

--- src/Models/ItemList.java
+++ src/Models/ItemList.java
@@ -1,15 +1,30 @@
 import java.util.ArrayList;
 
 public class ItemList {
-    private ArrayList<Item> items = new ArrayList<Item>();
-    public ItemList() {
-	items.add(0, new Biscuit());
-	items.add(new Salmon());
-    }
-    public Item getItem(int i) {
-	return items.get(i);
-    }
-    public int getSize() {
-	return items.size();
-    }
+	
+	/**
+	 * Default Constructor. Creates a ItemList containing a biscut and salmon.
+	 */
+	public ItemList() {
+		items.add(0, new Biscuit());
+		items.add(new Salmon());
+	}
+	
+	/**
+	 * @return the ith item on the list
+	 */
+	public Item getItem(int i) {
+		return items.get(i);
+	}
+	
+	/**
+	 * @return the size of the list
+	 */
+	public int getSize() {
+		return items.size();
+	}
+	
+	//private vars
+	private ArrayList<Item> items = new ArrayList<Item>();
+	
 }
